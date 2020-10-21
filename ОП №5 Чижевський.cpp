1512445d08695ccba0b5b2c228a77a0716880cd0
TheLastChance23 <chijevsky.maxim233@gmail.com>
2020-10-21 21:42:01 +0300

    Тест 1

---
+++ ОП №5 Чижевський.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char x;
+	cout << "Введіть букву ноти" << endl;
+	cin >> x;
+
+	switch (x)
+	{
+	case 'C': cout << "До\n"; break;
+	case 'D': cout << "Ре\n"; break;
+	case 'E': cout << "Мі\n"; break;
+	case 'F': cout << "Фа\n"; break;
+	case 'G': cout << "Соль\n"; break;
+	case 'A': cout << "Ля\n"; break;
+	case 'H': cout << "Сі\n"; break;
+	default: cout << "Такої ноти не існує" << endl; break;
+	}
+	system("pause");
+	return 0;
+}
