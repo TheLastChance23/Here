3b0a7d8fb9afe2a3d0d71ff29852118f52dfca88
TheLastChance23 <chijevsky.maxim233@gmail.com>
2020-10-26 09:47:21 +0200

    Проверка для "ЛР-5 ОПИ Чижевський"

---
+++ ОП №5 Чижевський.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+#include "windows.h"
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	char x;
+	cout << "Введіть букву ноти" << endl;
+	cin >> x;
+
+	switch (x)
+	{
+	case 'C': cout << "До\n"; break;
+	case 'D': cout << "Ре\n"; break;
+	case 'E': cout << "Мі\n"; break;
+	case 'F': cout << "Фа\n"; break;
+	case 'G': cout << "Соль\n"; break;
+	case 'A': cout << "Ля\n"; break;
+	case 'H': cout << "Сі\n"; break;
+	case '#': cout << "Работа с ЛР-5 ОПИ" << endl; break;
+	default: cout << "Такої ноти не існує" << endl; break;
+	}
+	system("pause");
+	return 0;
+}
